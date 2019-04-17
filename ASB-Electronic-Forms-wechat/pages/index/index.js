cb41e2eeb1b4855a322cf1d59f19adb003ce8243
KangVictor <orisho0330@gmail.com>
2019-04-17 23:17:11 +0800

    added inputBox for name, submit button. Added server '/input'

--- ASB-Electronic-Forms-wechat/pages/index/index.js
+++ ASB-Electronic-Forms-wechat/pages/index/index.js
@@ -59,5 +59,61 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  //input
+  data: {
+    focus: false,
+    inputValue: ''
+  },
+  bindKeyInput: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+  onInput: function () {
+    const murl = 'http://localhost:5000/input';
+    var inputBox = wx.createSelectorQuery();
+    inputBox.select('#inputBox');
+    // const a = inputBox.inputValue;
+    const mbody = JSON.stringify({ "hi": inputBox.inputValue});
+    debugger
+    wx.request({
+      url: murl,
+      method: 'POST',
+      data: mbody,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      success: function (res) {
+        console.log(res);
+        this.setData({ code: res.data });
+      }.bind(this)
+    });
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
 })
--- server.py
+++ server.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request,jsonify
 import random
 app = Flask(__name__)
 
 @app.route('/test', methods=['GET'])
 def test():
-	return str(random.randint(1,10000))
+	return str(random.randint(10000,99999))
+
+@app.route('/input', methods=['POST'])
+def input():
+	if request.is_json:
+		getInput = request.get_json()
+		print getInput
+		print "hi"
+		return "hello"
+		# return str(getInput['hi'])
