505840ab342e10b0b35c690d76c03181d23b5f72
major <echolabstech@gmail.com>
2019-04-22 18:35:03 +0800

    pof /create/order endpoint

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from flask import Flask, request,jsonify
 import random
 app = Flask(__name__)
 
-@app.route('/input', methods=['POST'])
-def input():
+@app.route('/create/order', methods=['POST'])
+def create_order():
 	if request.is_json:
 		wxBuyerInput = request.get_json()
 		num = random.randint(10000,99999)
