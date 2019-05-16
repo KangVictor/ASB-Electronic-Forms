ec9b7264f200593a9bbebc76f5a99b8c0c152a7b
Victor Kang <orisho0330@gmail.com>
2019-05-16 21:06:14 +0800

    Added swiper images and user checking function in index page

--- ASB-Electronic-Forms-wechat/pages/index/index.js
+++ ASB-Electronic-Forms-wechat/pages/index/index.js
@@ -3,45 +3,55 @@ Page({
 
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    adminAccounts: ["https://wx.qlogo.cn/mmopen/vi_32/xXh05DXhjejibarmQN1Nc3TgctlEfUVr8nbUSwaeuGmor9icWOsK3BwUSLhs3yFTUKu5lxFLfAOSlVMBfLTJFccA/132"], // admin accounts' avatarUrl
-    adminButtonDisabled: false // if admin = false, if nonadmin = true
+    adminButtonDisabled: true, // if admin = false, if nonadmin = true
+    indicatorDots: true,
+    autoplay: true,
+    circular: true,
+    interval: 2000,
+    duration: 1000,
+    imgUrls: ['/itemImg/singleWG.jpg', '/itemImg/doubleWG.jpg', '/itemImg/classicWG.jpg'],
+    backColor: '#ffffff',
+    whiteFont: '#ffffff'
   },
 
   onLoad: function() {
-    // wx.cloud.init()
-    // wx.cloud.callFunction({
-    //   name: 'getUserInfo',
-    //   success: function (res) {
-    //     if(res.unionid == 'Victor030330'){
-    //       adminButtonDisabled = false
-    //     }
-    //   },
-    //   fail: function (res){
-    //     console.log('fail')
-    //   }
-    // })
+    var getOpenId=''
     wx.login({
       success(res) {
         if (res.code) {
-          // 发起网络请求
           wx.request({
             url: `https://api.weixin.qq.com/sns/jscode2session?appid=wx3766d060f51ec08a&secret=4479f86f657f87374e1b5226519cd73b&js_code=${res.code}&grant_type=authorization_code`,
-            data: {
-              code: res.code
-            },
+            data: { code: res.code },
             method: 'GET',
-            header: {
-              'content-type': 'application/json' // 默认值
-            },
+            header: { 'content-type': 'application/json'  },
             success(res) {
-              console.log(res.data)
+              getOpenId = res.data.openid
             }
           })
-        } else {
-          console.log('登录失败！' + res.errMsg)
         }
       }
     })
+
+    var adminAccounts = []
+    wx.cloud.init()
+    wx.cloud.callFunction({
+      name: 'getWhiteList',
+      success: function (res) {
+          adminAccounts = res.result.data
+      },
+    })
+
+    setTimeout(function () {
+      console.log(adminAccounts)
+      console.log(getOpenId)
+      if(isAdmin(adminAccounts, getOpenId)){
+        this.setData({
+          adminButtonDisabled: false,
+          backColor:'#03A9AC'
+        })
+      }
+    }.bind(this), 5000)
+
     // change navigation bar color
     wx.setNavigationBarColor({
       frontColor: '#ffffff',
@@ -56,51 +66,12 @@ Page({
     })
   },
 
-  onReady: function(){
-    wx.getSetting({
-      success(res) {
-        if (res.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            success(res) {
-              console.log(res.userInfo)
-            }
-          })
-        }
-      }
-    })
-  },
-
-  bindGetUserInfo(e) {
+  onAdminButton() {
     wx.navigateTo({
-      url: '/pages/adminLoginPage/adminLoginPage?'
+      url: '/pages/checkOrderPage/checkOrderPage?'
     });
 
-    // check if the user is admin(ASB member)
-    // var nonAdmin = false;
-    // wx.getUserInfo({
-    //   success(res) {
-    //     console.log(res.userInfo);
-    //   }
-    // })
-    // for(var i = 0; i < this.data.adminAccounts.length; i++) {
-    //   if (e.detail.userInfo['avatarUrl'] == this.data.adminAccounts[i]) { // if not admin, disable the admin button
-    //     nonAdmin = false;
-    //     console.log('is Admin');
-    //   }
-    // }
-    // this.setData({ adminButtonDisabled: nonAdmin });
-    // if(nonAdmin == true) {
-    //   wx.showModal({
-    //     title: 'error',
-    //     content: 'Only ASB members can use this function',
-    //     showCancel: false,
-    //     confirmText: 'Ok'
-    //   })
-    // } else {
-      // wx.navigateTo({ // later create choosing page that can choose between file access
-      //   url: '/pages/checkOrderPage/checkOrderPage?'
-      // });
-    // }
+    
   },
 
   bindPurchase: function () {
@@ -109,3 +80,13 @@ Page({
     });
   },
 })
+
+function isAdmin(adminList, userOpenId) {
+  var check = false
+  for(var i = 0; i < adminList.length; i++){
+    if(userOpenId == adminList[i]['openId']) {
+      check = true
+    }
+  }
+  return check
+}
