846d21c3ed406d64b62eb929be0592f4eba90723
Major Sapp III <echolabstech@gmail.com>
2019-10-25 18:54:10 +0800

    Refactor asb order reservations page and smart search (#12)
    * Order can be confirmed by tapping. unconfirm works in same way as before
    
    * Changed design in purchase page.
    Multicreated item boxes, can create more items easier
    
    * Erased unnecessary showNoNegativeModal function
    non numbers are not allowed
    
    * erased genderdemographic
    
    * Fixed error of not sending quantity
    
    * CheckReservation page UI improvement.
    Box style similar to Reservation page
    Acronyms for readability
    
    * Fixed error in changing grade and class in reservationPage.
    Changed grade and class was not applied in the change
    
    * Fixed cloud function, update order by getting the confirmed variable from the function call
    
    * updates the order, but does not update the UI after the update
    
    * Changed name of the event of pressing confirm button
    
    * Use single array for the reservation. Simplify codes for getting the recent reservations for showReservation
    
    * Paging of reservations
    
    * Smarter search implemented. Erased search button, unconfirm button from the UI
    
    * Show toast box instead of modal when no reservation is found
    
    * Move the item text 30rpx left
    Move the inputbox 25rpx down
    
    * Changed back to scroll view
    Remove logic of next, previous, scroll new item, etc)
    
    * Only 10 items are shown when nothing is searched. When searched, all of the orders are shown.

--- ASB-Electronic-Forms-wechat/pages/checkReservationPage/checkReservationPage.js
+++ ASB-Electronic-Forms-wechat/pages/checkReservationPage/checkReservationPage.js
@@ -7,7 +7,10 @@ Page({
     itemNum: 3,
     itemNames: ['Single Tube Watergun', 'Double Tube Watergun', 'Classic Watergun'],
     keyword:'',
-    showNum: 5
+    currentPage: 0,
+    numResPerPage: 10,
+    hasNext: true,
+    hasPrev: false
   },
 
   onLoad: function() {
@@ -37,19 +40,12 @@ Page({
         console.log(getReservation);
         this.setData({ // give reservation the the entire data of reservation
           reservation: getReservation,
-        })
-        
-        var recent = []; // recent 15 reservation are shown
-        for (var i = 0; i < 15; i++) {
-          recent[i] = getReservation[getReservation.length - i - 1]
-        }
-        this.setData({
-          showReservation: recent
+          showReservation: getReservation.slice(0, this.data.numResPerPage)
         })
 
         wx.hideLoading()
       }.bind(this),
-      fail: function () {
+      fail: function() {
         wx.navigateTo({
           url: '/pages/serverFailPage/serverFailPage?'
         });
@@ -57,33 +53,26 @@ Page({
     })
   },
 
-  bindKeyInput: function(e) {
+  bindSearchInput: function(e) {
+    // change the keyword
     this.setData({
       keyword: e.detail.value
     })
-  },
-
-  searchReservation: function() {
+    // search
     if (this.data.keyword == '') {// if keyword(name, code) is blank just show all reservation
-      this.setData({showReservation: this.data.reservation})
+      const getReservation = this.data.reservation.slice(0,this.data.numResPerPage)
+      this.setData({ showReservation: getReservation })
     } else {
-      console.log('in')
       const foundreservation = findReservation(this.data.reservation, (this.data.keyword).toLowerCase());
       console.log(foundreservation);
       if (foundreservation.length == 0) {
-        wx.showModal({
+        wx.showToast({
           title: 'not found',
-          content: 'Buyer name not found',
-          confirmText: 'Ok',
-          showCancel: false
+          icon: 'none',
+          duration: 800
         })
-        console.log("buyer not found")
       } else {
-        this.setData({
-          showReservation: [],
-          
-        })
-        this.setData({showReservation:foundreservation})
+        this.setData({ showReservation: foundreservation })
       }
     }
   },
@@ -94,58 +83,42 @@ Page({
       title: 'Loading',
     })
     // find the confirmed variable
-    var index
-    for(var i = 0; i < this.data.reservation.length; i++){
-      if(this.data.reservation[i]._id == event.currentTarget.id) {
-        index = i
-        break
-      }
-    }
+    
+    const matchingReservation = findMatchingReservation(event.currentTarget.id, this.data.reservation)
+
     // confirm the selected reservation
     wx.cloud.callFunction({
       name: "confirmReservation",
       data: {
         reservationId: Number.parseInt(event.currentTarget.id),
-        confirmed: this.data.reservation[index].confirmed,
+        confirmed: matchingReservation.confirmed,
       },
       success: function (res) {
-        this.data.reservation[index].confirmed = !this.data.reservation[index].confirmed
-        for (var i = 0; i < this.data.showReservation.length; i++) {
-          if (this.data.showReservation[i]._id == event.currentTarget.id) {
-            this.data.showReservation[i].confirmed == !this.data.showReservation[i].confirmed
-            break
-          }
-        }
+        matchingReservation.confirmed = !matchingReservation.confirmed
+        this.setData({
+          showReservation: this.data.reservation.slice(0, 14)
+        })
         wx.hideLoading()
       }.bind(this)
     })
-  }
+  },
 })
 
-function findReservation(reservation, keyword) { // finds Reservation either by code or name
-  var foundreservation = [];
-  var count = 0;
-
-  if(hasNumber(keyword)){ // checks if the keyWord is a code
-  // find the code if so
-    for (var i = 0; i < reservation.length; i++) {
-      if (reservation[i]['_id'] == Number(keyword)) {
-        foundreservation[count] = reservation[i]
-        count ++;
-      }
-    }
+function findReservation(reservations, keyword) {
+  let foundReservations
+  if (hasNumber(keyword)) { // checks if the keyWord is a code
+    // find the code if so
+    foundReservations = reservations.filter((reservation) => { return (String(reservation._id)).includes(keyword); })
   } else { // find the buyer's name
-    console.log(reservation)
-    for (var i = 0; i < reservation.length; i++) {
-      if (reservation[i]['studentName'] == keyword){
-        foundreservation[count] = reservation[i];
-        count++;
-      }
-    }
+    foundReservations = reservations.filter((reservation) => { return reservation.studentName.includes(keyword); })
   }
-  return foundreservation;
+  return foundReservations
 }
 
 function hasNumber(myString) { // Check if the name contains integer
   return /\d/.test(myString);
+}
+
+function findMatchingReservation(id, model) {
+  return model.find(element => {return element._id == id})
 }
