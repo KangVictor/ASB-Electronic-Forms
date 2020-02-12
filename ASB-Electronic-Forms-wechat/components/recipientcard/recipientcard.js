1278809e1b9263293ab89bca0835ac898f13f70f
KangVictor <orisho0330@gmail.com>
2020-02-12 15:44:09 +0800

    New recipients are being added. RecipientCard must be implemented

--- ASB-Electronic-Forms-wechat/components/recipientcard/recipientcard.js
+++ ASB-Electronic-Forms-wechat/components/recipientcard/recipientcard.js
@@ -8,6 +8,14 @@ Component({
       type: String,
       value: 'John'
     },
+    recipientGrade: {
+      type: Number,
+      value: 9
+    },
+    recipientClass: {
+      type: Number,
+      value: 1
+    },
     quantity:{
       type: Number,
       value: 1
@@ -19,7 +27,10 @@ Component({
    * Component initial data
    */
   data: {
-
+    arrayGrade: ['9', '10'],
+    indexGrade: '0',
+    arrayClass: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
+    indexClass: '0',
   },
 
   /**
