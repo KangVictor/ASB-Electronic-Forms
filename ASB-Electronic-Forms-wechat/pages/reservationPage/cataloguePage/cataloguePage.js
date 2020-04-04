53da7857c89a2d2da1c4221be7209289dc93586e
Victor Kang <orisho0330@gmail.com>
2020-04-04 17:27:11 +0800

    Changed item values and orders correctly

--- ASB-Electronic-Forms-wechat/pages/reservationPage/cataloguePage/cataloguePage.js
+++ ASB-Electronic-Forms-wechat/pages/reservationPage/cataloguePage/cataloguePage.js
@@ -28,11 +28,11 @@ Page({
       {name: 'Champagne Rose', quantity: 0, value: 5, imgUrl: '/resources/520/champagne-rose.jpg', itemNum: 1},
       {name: 'Succulent Pot', quantity: 0, value: 6, imgUrl: '/resources/520/succuelnt-pot.jpg', itemNum: 2},
       {name: 'Sunflower', quantity: 0, value: 10, imgUrl: '/resources/520/sunflower.jpg', itemNum:3},
-      {name: '11 Red Rose Bouquet', quantity: 0, value: 60, imgUrl: '/resources/520/11-red-rose-bouquet.jpg', itemNum: 4},
-      {name: '11 Champagne Rose Bouquet', quantity: 0, value: 65, imgUrl: '/resources/520/11-champagne-rose-bouquet.jpg', itemNum: 5},
-      {name: 'Daisy Bouquet', quantity: 0, value: 35, imgUrl: '/resources/520/daisy-bouquet.jpg', itemNum: 6},
-      {name: '99 Rose Bouquet', quantity: 0, value: 450, imgUrl: '/resources/520/99-rose-bouquet.jpg', itemNum: 7},
-      {name: '6 Sunflower Bouquet', quantity: 0, value: 10, imgUrl: '/resources/520/6-sunflower-bouquet.jpg', itemNum: 8}
+      {name: 'Daisy Bouquet', quantity: 0, value: 35, imgUrl: '/resources/520/daisy-bouquet.jpg', itemNum: 4},
+      {name: '6 Sunflower Bouquet', quantity: 0, value: 55, imgUrl: '/resources/520/6-sunflower-bouquet.jpg', itemNum:5},
+      {name: '11 Red Rose Bouquet', quantity: 0, value: 60, imgUrl: '/resources/520/11-red-rose-bouquet.jpg', itemNum: 6},
+      {name: '11 Champagne Rose Bouquet', quantity: 0, value: 65, imgUrl: '/resources/520/11-champagne-rose-bouquet.jpg', itemNum: 7},
+      {name: '99 Rose Bouquet', quantity: 0, value: 450, imgUrl: '/resources/520/99-rose-bouquet.jpg', itemNum: 8},
     ],
     recipients: [ // format: { rName: 'a', rGrade: 9, rItem: 0, rClass: 1, rQuantity: 1 }
     ],
@@ -220,6 +220,7 @@ Page({
         const studentCl = this.data.studentClass
         const studentGr = this.data.studentGrade
         const studentTo = this.data.total
+        
 
         const tempRecipientList = this.data.recipients
         var recipientLi = []
